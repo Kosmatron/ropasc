2c705ca7e8a0fcdcbc302ccd7601c540311ee9cf
Tronkosma <129993289+Kosmatron@users.noreply.github.com>
2023-04-26 10:14:35 +0200

    Add 5 rounds to the game, declarations and "style"

--- script.js
+++ script.js
@@ -1,33 +1,132 @@
 function getComputerChoice(arr) {
     const randomChoice = Math.floor(Math.random() * arr.length);  //get random index value from array
-    const computerValue = arr[randomChoice]; //get random item from array from the index value (?)
-    return computerValue; 
+    const computerValue = arr[randomChoice]; //get item from array from the random index value (?)
+    return computerValue;
 }
 
 const array = ["Rock", "Paper", "Scissors"]; //the values
 const computerChoice = getComputerChoice(array)
 
-//console.log(computerChoice)
 
-const playerChoice = "Rock"
+//const playerChoice = "Paper"
+
+let playerScore = 0;
+let computerScore = 0;
+let currentRound = 0;
+
 
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection == computerSelection) {
-        return "It's a tie!"
-} else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-    return "You win!"
-} else if (playerSelection == "Paper" && computerSelection == "Rock") {
-    return "You win!"
-} else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-    return "You win!"
-} else if (playerSelection == "Rock" && computerSelection == "Paper") {
-    return "You lose!"
-} else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-    return "You lose!"
-} else (playerSelection == "Scissors" && computerSelection == "Rock") 
-    return "You lose!"
+        currentRound = currentRound + 1;
+        return console.log("It's a tie!")
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerScore = playerScore + 1;
+        currentRound = currentRound + 1;
+        return console.log("You win!")
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerScore = playerScore + 1;
+        currentRound = currentRound + 1;
+        return console.log("You win!")
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore = playerScore + 1;
+        currentRound = currentRound + 1;
+        return console.log("You win!")
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore = computerScore + 1;
+        currentRound = currentRound + 1;
+        return console.log("You lose!")
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore = computerScore + 1;
+        currentRound = currentRound + 1;
+        return console.log("You lose!")
+    } else (playerSelection == "scissors" && computerSelection == "rock")
+    computerScore = computerScore + 1;
+    currentRound = currentRound + 1;
+    return console.log("You lose!")
+
+}
+
+function getUserPrompt() {
+    const playerInput = prompt("Type either Rock, Paper or Scissors!")
+    if (playerInput.toLowerCase() == "rock" || playerInput.toLowerCase() == "paper" || playerInput.toLowerCase() == "scissors") {
+        return playerInput
+    } else {
+        getUserPrompt()
+    }
+}
+
+
+
+
+
+function getUserPromptAndPlayRound() {
+    const playerInput = getUserPrompt()
+    const computerInput = getComputerChoice(array)
+    const matchResult = playRound(playerInput.toLowerCase(), computerInput.toLowerCase())
+}
+
+
+function declareWinner() {
+    const roundScore = playRound(playerInput.toLowerCase(), computerInput.toLowerCase())
+    return roundScore
+}
+
+
+
+
+function game() {
+
+
+    getUserPromptAndPlayRound()
+
+    console.log("Your score: " + playerScore)
+    console.log("Computer score: " + computerScore)
+    console.log("Current round: " + currentRound)
+    console.log("------------------------------")
+
+    getUserPromptAndPlayRound()
+
+    console.log("Your score: " + playerScore)
+    console.log("Computer score: " + computerScore)
+    console.log("Current round: " + currentRound)
+    console.log("------------------------------")
+
+    getUserPromptAndPlayRound()
+
+    console.log("Your score: " + playerScore)
+    console.log("Computer score: " + computerScore)
+    console.log("Current round: " + currentRound)
+    console.log("------------------------------")
+
+    getUserPromptAndPlayRound()
+
+    console.log("Your score: " + playerScore)
+    console.log("Computer score: " + computerScore)
+    console.log("Current round: " + currentRound)
+    console.log("------------------------------")
+
+    getUserPromptAndPlayRound()
+
+    console.log("Your score: " + playerScore)
+    console.log("Computer score: " + computerScore)
+    console.log("Current round: " + currentRound)
+    console.log("------------------------------")
 
 }
 
 
-console.log(playRound(playerChoice, computerChoice))
+function gameEnd() {
+    if (currentRound == 5 && playerScore == computerScore) {
+        return alert("The game is tied!")
+    } else if (currentRound == 5 && playerScore > computerScore) {
+        return alert("You won the game!")
+    } else if (currentRound == 5 && playerScore < computerScore) {
+        return alert("You lost the game!")
+    }
+}
+
+game()
+gameEnd()
+
+
